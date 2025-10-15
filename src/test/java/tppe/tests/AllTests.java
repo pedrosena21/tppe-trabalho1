32dbd0df2430c4ff2400a44426d7d1e9365ea8bd
Guilherme Brito de Souza <guilhermebritodesouza2308@gmail.com>
2025-10-15 18:01:40 -0300

    Adição das pastas de test

---
+++ src/test/java/tppe/tests/AllTests.java
@@ -0,0 +1,13 @@
+package tppe.tests;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+    SorteioRodadasTest.class
+    
+})
+class AllTests {
+   
+}
---
+++ src/test/java/tppe/tests/SorteioRodadasTest.java
@@ -0,0 +1,5 @@
+package tppe.tests;
+
+public class SorteioRodadasTest {
+
+}
