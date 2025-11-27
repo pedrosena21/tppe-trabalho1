e417670a9dbb90ca9ef4b6b2f9ec1ac78e9d43bb
Guilherme Brito de Souza <guilhermebritodesouza2308@gmail.com>
2025-11-27 18:38:06 -0300

    [Refact] Campeonato::calcularPontuacaoTime, Campeonato

--- src/main/java/com/campeonato/CalculadoraPontuacaoTime.java
+++ src/main/java/com/campeonato/CalculadoraPontuacaoTime.java
@@ -11,40 +11,33 @@ public class CalculadoraPontuacaoTime {
     }
 
     public short calcular() {
-        short pontuacao = 0;
-		int golsTime = 0;
-		int golsAdversario = 0;
+        short pontuacaoTotal = 0;
 
 		for (Rodada rodada : campeonato.getRodadas()) {
 			for (Partida partida : rodada.getPartidas()) {
 
-				if (partida.getTimeCasa().equals(time)) {
-					golsTime = partida.getQuantidadeGols(partida.getTimeCasa());
-					golsAdversario = partida.getQuantidadeGols(partida.getTimeVisitante());
-
-					pontuacao += calcularPontuacaoPartida(pontuacao, golsTime, golsAdversario);
-
-				}
-
-				if (partida.getTimeVisitante().equals(time)) {
-					golsTime = partida.getQuantidadeGols(partida.getTimeVisitante());
-					golsAdversario = partida.getQuantidadeGols(partida.getTimeCasa());
-
-					pontuacao += calcularPontuacaoPartida(pontuacao, golsTime, golsAdversario);
+				if (partida.getTimeCasa().equals(time) || partida.getTimeVisitante().equals(time)) {
+					Time adversario = partida.getTimeCasa().equals(time) ? partida.getTimeVisitante() : partida.getTimeCasa();
+					
+					int golsTime = partida.getQuantidadeGols(time);
+					int golsAdversario = partida.getQuantidadeGols(adversario);
+					
+					pontuacaoTotal += calcularPontuacaoPartida(golsTime, golsAdversario);
 				}
 			}
 		}
 
-		return pontuacao;
+		return pontuacaoTotal;
     }
     
-    private short calcularPontuacaoPartida(short pontuacao, int golsTime, int golsAdversario) {
+    private short calcularPontuacaoPartida(int golsTime, int golsAdversario) {
 		if (golsTime > golsAdversario) {
-			pontuacao += 3;
+			return 3;
 		} else if (golsTime == golsAdversario) {
-			pontuacao += 1;
+			return 1;
+		}else {
+			return 0;
 		}
-		return pontuacao;
 	}
 
 }
